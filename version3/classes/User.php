830bf40806282b86ac129a5a9002a7cae142bb6a
Daniel Schwen <danielschwen@gmail.com>
2021-04-09 15:36:33 -0500

    Adding new functions to Users

--- version3/classes/User.php
+++ version3/classes/User.php
@@ -9,12 +9,12 @@ class User extends Database {
     public $error;
     
     // Check to see if the user is able to log into the system
-    function login($con, $userID, $password) {
+    function login($con, $username, $password) {
         
         // Perform a MySQL search to see if that user exists in the users table
         $stmt = mysqli_query($con, "SELECT *
                                     FROM users
-                                    WHERE user_id = '$userID'");
+                                    WHERE username = '$username'");
         $row = mysqli_fetch_array($stmt);
         
         // Determine if the MySQL search found the user
@@ -33,14 +33,14 @@ class User extends Database {
                 // Set the session for the user
                 session_start();
                 $_SESSION['login'] = "yes";
-                $_SESSION['User_ID'] = $row['user_id'];
-                $_SESSION['User_Passcode'] = $password;
-                $_SESSION['First_Name'] = $row['first_name'];
-                $_SESSION['Last_Name'] = $row['last_name'];
+                $_SESSION['Username'] = $row['username'];
+                $_SESSION['Password'] = $password;
+                $_SESSION['First_Name'] = $row['f_name'];
+                $_SESSION['Last_Name'] = $row['l_name'];
                 $_SESSION['Email'] = $row['email'];
-                $_SESSION['Phone_Num'] = $row['phone_number'];
+                $_SESSION['Phone_Num'] = $row['phone_num'];
                 $_SESSION['Department'] = $row['department'];
-                $_SESSION['Position'] = $row['position'];
+                $_SESSION['Title'] = $row['title'];
 
                 return true;
             }
@@ -48,13 +48,13 @@ class User extends Database {
     }
 
     // Add a new user to the DB
-    function addUser($con, $uID, $psw, $fName, $lName, $email, $phone, $dept, $position) {
+    function addUser($con, $username, $psw, $fName, $lName, $email, $phone, $dept, $title) {
 
         // Can't store plaintext passwords in the DB
         $passcode = password_hash($psw, PASSWORD_DEFAULT);
         
-        $sql = "INSERT INTO users (user_id, password, first_name, last_name, email, phone_number, department, position)
-        VALUES ('$uID', '$passcode', '$fName', '$lName', '$email', '$phone', '$dept', '$position')";
+        $sql = "INSERT INTO users (username, password, f_name, l_name, email, phone_num, department, title)
+        VALUES ('$username', '$passcode', '$fName', '$lName', '$email', '$phone', '$dept', '$title')";
 
         if (mysqli_query($con, $sql)) {
             // User was successfully added to the DB
@@ -66,25 +66,80 @@ class User extends Database {
         }
     }
 
-    // Get all the users from a specific department
-    function getDepartment($con, $department) {
+    // Flag the database that the user is new and needs to change their password AND set their security questions
+    function flagNewUser($con, $username) {
+
+        $sql = "INSERT INTO userstatus (username)
+                VALUES ('$username')";
         
-        $query = "SELECT user_id 
-            FROM users 
-            WHERE department='$department'";
-        
-        if ($result = mysqli_query($con, $query)) {
-            return $result;
+        if (mysqli_query($con, $sql)) {
+            // User was successfully flagged as a new user in the DB
+            return "Success";
         } else {
-            $this->error = "Unable to process department query: " . mysql_error();
+            // Couldn't flag the user, return the error
+            $this->error = "Unable to add user: " . mysqli_error($con);
             return NULL;
         }
     }
 
+    /*
+     * If a user cannot remember their password, we will provide them with a way to set their own security questions 
+     * and answers. This way, they can recover their password on their own. If they are unable to answer their own
+     * security questions, they will need to reach out to their supervisor/manager, who can then submit a ticket for
+     * a password request on the user's behalf.
+    */
+    function setQuestions($con, $username, $q1, $q2, $q3, $q4, $q5) {
+
+    }
+
+    /*
+     * Both the security questions (above) and the answers to the questions (below) will be encrypted/decrypted using
+     * a Cryptography Exstension called OpenSSL. Here's where I found out how to do this:
+     * 
+     * https://www.geeksforgeeks.org/how-to-encrypt-and-decrypt-a-php-string/
+     * 
+    */
+    function setAnswers($con, $username, $ans1, $ans2, $ans3, $ans4, $ans5) {
+
+    }
+
+    // Flag the database that the user needs to reset their password
+    function flagPswdReset($con, $username) {
+
+        $sql = "UPDATE userstatus
+                SET password_reset = 'Yes'
+                WHERE userstatus.username = '$username'";
+        
+        if (mysqli_query($con, $sql)) {
+            // User was successfully flagged as needing to change their password in the DB
+            return "Success";
+        } else {
+            // Couldn't flag the user, return the error
+            $this->error = "Unable to add user: " . mysqli_error($con);
+            return NULL;
+        }
+    }
+
+    // TODO: make sure I don't ACTUALLY need this function anymore...
+    // // Get all the users from a specific department
+    // function getDepartment($con, $department) {
+        
+    //     $query = "SELECT username 
+    //         FROM users 
+    //         WHERE department='$department'";
+        
+    //     if ($result = mysqli_query($con, $query)) {
+    //         return $result;
+    //     } else {
+    //         $this->error = "Unable to process department query: " . mysql_error();
+    //         return NULL;
+    //     }
+    // }
+
     // Get all of the users from the DB
     function getAllUsers($con) {
 
-        $query = "SELECT user_id, first_name, last_name, email, phone_number, department, position 
+        $query = "SELECT username, f_name, l_name, email, phone_num, department, title 
         FROM users";
 
         if ($result = mysqli_query($con, $query)) {
@@ -96,13 +151,13 @@ class User extends Database {
     }
 
     // Change the user's password
-    function changePassword($con, $userID, $password) {
+    function changePassword($con, $username, $password) {
 
         $hash = password_hash($password, PASSWORD_DEFAULT);
 
         $sql = "UPDATE users
                 SET password = '$hash'
-                WHERE users.user_id = '$userID'";
+                WHERE users.username = '$username'";
         
         if (mysqli_query($con, $sql)) {
             // Password was successfully updated
@@ -110,6 +165,7 @@ class User extends Database {
         } else {
             // Couldn't update the password
             $this->error = "Unable to update password: " . mysqli_error($con);
+            return NULL;
         }
     }
 
